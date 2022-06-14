3b5c0f141f29b6601360a26a0031e5667fa6bfe5
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2022-06-14 14:30:28 +0200

    Add new random query strategies.

--- src/active_learning/components/query_strategies.py
+++ src/active_learning/components/query_strategies.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta
 
+import numpy as np
+import pandas as pd
 from scipy import optimize
 
 from active_learning.components.sampling.latin_square import iterative_sampler
@@ -47,6 +49,40 @@ def reject_on_bounds_ada(X, y, active_function, size=10,
     selector = order <= size
     return x_new[selector], cov[selector]
 
+
+##############################################
+#  Random with probability density function  #
+##############################################
+
+
+DEFAULT_RNG = np.random.default_rng()
+
+def indices_of_random_sampling_in_finite_set(pdf, candidates, nb_samples, *, rng=DEFAULT_RNG):
+    """Pick `nb_samples` items among the candidates according to the probability density function `pdf`."""
+    probability = pdf(candidates)
+    probability /= np.sum(probability)
+    indices = pd.DataFrame(candidates).index
+    return rng.choice(indices, size=nb_samples, replace=False, p=probability)
+
+def random_sampling_in_finite_set(pdf, candidates, nb_samples, *, rng=DEFAULT_RNG):
+    """Pick `nb_samples` items among the candidates according to the probability density function `pdf`."""
+    probability = pdf(candidates)
+    probability /= np.sum(probability)
+    return rng.choice(candidates, size=nb_samples, replace=False, p=probability, axis=0)
+
+def random_sampling_in_domain(pdf, bounds, nb_samples, *, candidates_per_sample=50, rng=DEFAULT_RNG):
+    """Pick `nb_samples` items in the domain delimited by `bounds` according to the probability density function `pdf`."""
+    dimension = len(bounds[0])
+    candidates = (bounds[1] - bounds[0]) * rng.random((candidates_per_sample * nb_samples, dimension)) + bounds[0]
+    return random_sampling_in_finite_set(pdf, candidates, nb_samples, rng=rng)
+
+
+def random_query(X, y, active_function, size=10, batch_size=10, bounds=None, **args):
+    """Wrap the above function with the same API as the other query strategies."""
+    return random_sampling_in_domain(active_function, bounds, size, candidates_per_sample=batch_size//size)
+
+
+################
 
 if __name__ == '__main__':
     import numpy as np
