889a884e8fc8ad41e7698cbb4183e24c96476896
vlaurent <vlaurent@eurobios.com>
2022-10-17 18:03:58 +0200

    dev: removing empty files

--- benchmark/analyse.py
+++ benchmark/analyse.py
@@ -1,20 +1,34 @@
+import matplotlib
+
+try:
+    matplotlib.use("qt5agg")
+except:
+    pass
 import matplotlib.pyplot as plot
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
-from sklearn.model_selection import ShuffleSplit
 from sklearn.svm import SVR
 
-from benchmark.naive_2d import add_to_benchmark, run, plot_results
-from benchmark.utils import evaluate, eval_surf_2d
+from active_learning.components.active_criterion import VarianceBis
+from active_learning.components.query_strategies import Uniform, QueryVariancePDF
+from sklearn.model_selection import ShuffleSplit
+from active_learning.components.sampling import latin_square
+from active_learning.components.test import TestingClass
 from active_learning.data import functions
 from active_learning.models.smt_api import SurrogateKRG
+from benchmark.utils import evaluate, eval_surf_2d
+from sklearn.svm import SVC, SVR
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
 
 functions_ = list(functions.bounds.keys())
 __n_sim__ = 1
-name = "grammacy_lee_2009_rand"
+name = "marelli_2018"
 fun = functions.__dict__[name]
 
 estimator_parameters = {
@@ -33,29 +47,51 @@ def analyse_results(path):
     print(res)
 
 
+est = Pipeline([("est", KNeighborsClassifier())])
+est = Pipeline([("est", SVC(C=500))])
+est = Pipeline([("poly", PolynomialFeatures(degree=[1, 3])), ("est", LinearRegression())])
+est = Pipeline([("est", RandomForestRegressor(n_estimators=10, bootstrap=True, max_samples=0.9))])
+est = Pipeline([("est", SVC(C=50, gamma=1))])
+
+
 if __name__ == '__main__':
-    analyse_results("benchmark/results.csv")
+    n0 = 40
+    budget = 70
+    steps = 10
+
+    bounds = np.array(functions.bounds[fun])
+
+    def sampler(size):
+        return pd.DataFrame(latin_square.scipy_lhs_sampler(size=size, x_limits=bounds))
+
+    a = TestingClass(
+        budget, n0, fun,
+        VarianceBis(
+            estimator=est, splitter=ShuffleSplit(n_splits=4, train_size=0.9)),
+        QueryVariancePDF(
+            bounds, num_eval=int(4000)),
+        sampler, bounds=bounds, n_steps=steps)
+    p = TestingClass(
+        budget, n0, fun,
+        VarianceBis(
+            estimator=est, splitter=ShuffleSplit(n_splits=4, train_size=0.9)),
+        Uniform(bounds),
+
+        sampler, bounds=bounds, n_steps=steps)
+    a.run()
+    p.run()
 
     from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-    n0 = 41
-    n_step = 10
-    budget = n0 + n_step
-    a, p, r = None, None, None
-    for i in range(__n_sim__):
-        a, p, r = run(fun, n0=n0, name=name, budget=budget, n_step=n_step, estimator=estimator_parameters[1])
-        add_to_benchmark(r)
-
-    bounds = np.array(functions.bounds[fun])
     xx, yy, x, z = eval_surf_2d(fun, bounds, num=200)
 
     # X = active_learner.result[n_step + 1]["data"]
-    X = a.x_input
-    prediction = a.surface
+    X = a.learner.x_input
+    prediction = a.learner.result[8]["surface"]
     active_criterion = a.active_criterion
 
     zz = prediction(x)
-    zzz = p.surface(x)
+    zzz = p.learner.result[8]["surface"](x)
     std = active_criterion(x)
 
     fig, ax = plot.subplots(ncols=2)
@@ -81,7 +117,7 @@ if __name__ == '__main__':
 
     plot.figure()
     plot.pcolormesh(xx, yy, std.reshape(len(xx), len(yy)), cmap="rainbow")
-    x_new =a.x_new
+    x_new = a.learner.x_new
     plot.scatter(x_new[0], x_new[1], c="k")
     plot.scatter(X[0], X[1], c="b")
 
@@ -101,18 +137,15 @@ if __name__ == '__main__':
     plot.figure(figsize=(4, 4), dpi=200)
     plot.contour(xx, yy, z.reshape(len(xx), len(yy)), levels=10, linewidths=0.3,
                  colors='k')
-    x_new = a.scaling_method.transform(a.x_new)
-    plot.scatter(X[0], X[1], cmap="rainbow", c=X.index)
+
+    plot.scatter(a.learner.x_input[0], a.learner.x_input[1], cmap="rainbow", c=X.index, marker=".")
     # plot.scatter(x0[:, 0], x0[:, 1], c="k")
     # plot.scatter(passive_learner.result[1]["data"][0],
     #              passive_learner.result[1]["data"][1], c="k", marker="+")
-    eval_p = evaluate(fun, p.surface, bounds, num_mc=10000)
-    eval_a = evaluate(fun, a.surface, bounds, num_mc=10000)
+    eval_p = evaluate(fun, p.learner.result[8]["surface"], bounds, num_mc=10000)
+    eval_a = evaluate(fun, a.learner.result[8]["surface"], bounds, num_mc=10000)
     print(
         f"passive performance : {eval_p}\n"
         f"active  performance : {eval_a}")
     plot.tight_layout()
-    plot.savefig(f"benchmark/sampling_example_function_{name}.png")
-
-    plot.figure()
-    plot_results(n0=n0, function=name)
+    plot.savefig(f"benchmark/figures/sampling_example_function_{name}.png")
