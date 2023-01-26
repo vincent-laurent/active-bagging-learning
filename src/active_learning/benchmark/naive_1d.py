600c99eba857bd98ef5401c5028fef320c6bf4b2
vlaurent <vlaurent@eurobios.com>
2023-01-26 16:44:18 +0100

    doc : 1d

--- src/active_learning/benchmark/naive_1d.py
+++ src/active_learning/benchmark/naive_1d.py
@@ -12,18 +12,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sklearn.model_selection
-from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
-from sklearn.linear_model import Ridge
-from sklearn.neural_network import MLPRegressor
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import PolynomialFeatures
 
 from active_learning.components.active_criterion import GaussianProcessVariance, \
-    VarianceBis, VarianceEnsembleMethod
+    VarianceBis
 from active_learning.components.query_strategies import QueryVariancePDF
-from active_learning.components.test import TestingClass, plot_iter, analyse_1d
+from active_learning.components.test import TestingClass, plot_iter
 
 SEED = 1234
 RNG = np.random.default_rng(seed=SEED)
@@ -41,30 +36,7 @@ def sampler(n):
 
 
 kernel = 1 * RBF(0.01)
-xtra_trees = ExtraTreesRegressor(bootstrap=False, n_estimators=50)
-xtra_trees_b = ExtraTreesRegressor(bootstrap=True, n_estimators=50,
-                                   max_samples=0.7)
-spline_fitting = make_pipeline(PolynomialFeatures(100, include_bias=True),
-                               Ridge(alpha=1e-3))
 krg = GaussianProcessRegressor(kernel=kernel, )
-mlp = MLPRegressor(hidden_layer_sizes=(50, 4), max_iter=300)
-
-testing = TestingClass(
-    budget=500,
-    budget_0=10,
-    function=unknown_function,
-    active_criterion=VarianceEnsembleMethod(estimator=xtra_trees_b),
-    query_strategy=QueryVariancePDF(bounds, num_eval=50000),
-    x_sampler=sampler,
-    n_steps=50,
-    bounds=bounds
-
-)
-
-testing.run()
-plt.figure(figsize=(4, 4), dpi=200)
-testing.plot_error_vs_criterion_pointwise(num_mc=100)
-analyse_1d(testing)
 
 # ======================================================================================
 #
@@ -102,9 +74,7 @@ testing = TestingClass(
     unknown_function,
     GaussianProcessVariance(kernel=kernel),
     QueryVariancePDF(bounds, num_eval=2000),
-    sampler, steps, bounds=bounds
-
-)
+    sampler, steps, bounds=bounds)
 
 testing.run()
 
